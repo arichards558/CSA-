a68977fe9513998040131d82f0051a0fb30dbec6
arichards558 <31348479+arichards558@users.noreply.github.com>
2017-11-08 09:18:22 -0600

    Update

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    public static void main()
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Medialib/MediaLib.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class MediaLib here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MediaLib
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    public static void main()
+    {
+        System.out.println("Welcome to your media library!");
+    }
+        // put your code here
+       
+    
+}
+
